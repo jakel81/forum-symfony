526ccf2ee9bb9df4f458573176b1ba9da194b497
Sébastien Maloron <smaloron@moi.com>
2017-09-06 11:58:06 +0200

    Modification de la création des posts liaison entre l'auteur d'un post et l'utilisateur identifié

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -27,29 +27,39 @@ class DefaultController extends Controller
         $list = $repository->getAllThemes()->getArrayResult();
         $postListByYear = $postRepository->getPostsGroupedByYear();
 
-        //Création du formulaire
-        $post = new Post();
-        $post->setCreatedAt(new \DateTime());
-        $form = $this->createForm(PostType::class, $post);
+        //Gestion des nouveaux posts
+        $user = $this->getUser();
+        $roles = isset($user)?$user->getRoles():[];
+        $formView = null;
+        if(in_array("ROLE_AUTHOR", $roles)) {
+            //Création du formulaire
+            $post = new Post();
+            $post->setCreatedAt(new \DateTime());
+            $post->setAuthor($user);
+            $form = $this->createForm(PostType::class, $post);
 
-        //Hydratation de l'entité post
-        $form->handleRequest($request);
+            //Hydratation de l'entité post
+            $form->handleRequest($request);
 
-        //Traitement du formulaire
-        if($form->isSubmitted() and $form->isValid()){
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($post);
-            $em->flush();
+            //Traitement du formulaire
+            if ($form->isSubmitted() and $form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($post);
+                $em->flush();
 
-            //Redirection
-            return $this->redirectToRoute("homepage");
+                //Redirection
+                return $this->redirectToRoute("homepage");
+            }
+
+            $formView = $form->createView();
         }
+        //Fin de la gestion des nouveaux posts
 
         return $this->render('default/index.html.twig',
             [
                 "themeList" => $list,
                 "postList"=>$postListByYear,
-                "postForm" => $form->createView()
+                "postForm" => $formView
             ]);
     }
 
